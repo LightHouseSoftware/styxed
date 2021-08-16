1d1101fae37f9afc7ca2621fdecbe45a1d9942d1
Oleg Bakharev <disconnectix@gmail.com>
2021-08-16 05:12:05 +0300

    Removed old imports

--- source/styxed/package.d
+++ source/styxed/package.d
@@ -5,8 +5,4 @@ public {
 	import styxed.constants;
 	// basic working unit in 9P2000/Styx protocol
 	import styxed.message;
-	// client interfaces
-	import styxed.client;
-	// server interfaces
-	import styxed.server;
 }
